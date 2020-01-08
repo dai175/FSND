a499737629d5133113aab2351dea19dc3b1b8c89
Daisuke Ohba <dai175dai@gmail.com>
2020-01-08 21:28:31 +0900

    update BasicFlaskAuth and FlaskRecap

--- BasicFlaskAuth/app.py
+++ BasicFlaskAuth/app.py
@@ -7,9 +7,9 @@ from urllib.request import urlopen
 
 app = Flask(__name__)
 
-AUTH0_DOMAIN = @TODO_REPLACE_WITH_YOUR_DOMAIN
+AUTH0_DOMAIN = 'torajiro.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = @TODO_REPLACE_WITH_YOUR_API_AUDIENCE
+API_AUDIENCE = 'image'
 
 
 class AuthError(Exception):
@@ -54,6 +54,10 @@ def get_token_auth_header():
 def verify_decode_jwt(token):
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
+    # myurl = 'https://%s/.well-known/jwks.json' % (AUTH0_DOMAIN)
+    # jsonurl = urlopen(myurl)
+    # content = jsonurl.read().decode(jsonurl.headers.get_content_charset())
+    # jwks = json.loads(content)
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
     if 'kid' not in unverified_header:
@@ -104,21 +108,34 @@ def verify_decode_jwt(token):
                 'description': 'Unable to find the appropriate key.'
             }, 400)
 
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        abort(400)
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
+    if permission not in payload['permissions']:
+        abort(403)
 
-    return wrapper
+    return True
 
-@app.route('/headers')
-@requires_auth
-def headers(payload):
-    print(payload)
-    return 'Access Granted'
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
+
+            check_permissions(permission, payload)
+
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+    return requires_auth_decorator
+
+@app.route('/image')
+@requires_auth('get:images')
+def image(jwt):
+    print(jwt)
+    return 'not implement'
